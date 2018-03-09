4ff4328dc063a07a4a2411a6bb464a1be9422843
WebbZhang <WebbZhang@boyaa.com>
2018-03-09 16:36:49 +0800

    fix

--- NetFrame/Auto/LengthEncoding.cs
+++ NetFrame/Auto/LengthEncoding.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 namespace NetFramework.Auto
 {
    public class LengthEncoding
